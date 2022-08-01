767cd259dba736bc78680136fb44a63b6f2d9dd2
이시형 <tlgud585@gmail.com>
2022-08-01 22:53:34 +0900

    ADD : 메인 경로 추가 (#4)
    delete tailwindcss
    add 메인경로

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Clothes from './pages/clothes/Clothes';
+import Home from './pages/home/Home';
+import Meeting from './pages/meeting/Meeting';
+import Sky from './pages/sky/Sky';
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-pink-800'>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <GlobalStyles/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/today_clothes" element={<Clothes/>}/>
+        <Route path="/today_sky" element={<Sky/>}/>
+        <Route path="/today_meeting" element={<Meeting/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 

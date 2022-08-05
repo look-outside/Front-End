df081acb096d227ec2d84a28df2b83a9087dcd88
이시형 <tlgud585@gmail.com>
2022-08-06 04:00:43 +0900

    ADD :  아이디 찾기 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
-import Comments from "./components/myPage/Comments";
+// import Comments from "./components/myPage/Comments";
 import Delete from "./components/myPage/Delete";
 import Info from "./components/myPage/Info";
-import Posts from "./components/myPage/Posts";
+// import Posts from "./components/myPage/Posts";
 import Clothes from "./pages/clothes/Clothes";
+import FindId from "./pages/find_id/FindId";
 
 import Home from "./pages/home/Home";
 import Join from "./pages/Join/Join";
@@ -27,11 +28,13 @@ function App() {
 					<Route path="today_meeting" element={<Meeting />}/>
 					<Route path="login" element={<Login/>}/>
 					<Route path="join" element={<Join/>}/>
-
+					<Route path="find">
+						<Route path="id" element={<FindId/>}/>
+					</Route>
 					<Route path="my_page" element={<MyPage />}>
 						<Route path='info' element={<Info />} />
-						<Route path='posts' element={<Posts/>} />
-						<Route path='comments' element={<Comments/>} />
+						{/* <Route path='posts' element={<Posts/>} />
+						<Route path='comments' element={<Comments/>} /> */}
 						<Route path='delete' element={<Delete/>} />
 					</Route>
 

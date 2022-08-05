959aed83d6783a81441792d3b3bfe611a570ebf3
이시형 <tlgud585@gmail.com>
2022-08-05 23:56:23 +0900

    ADD : use-input
    use-input 커스텀 훅 추가

---
+++ src/hooks/use-input.ts
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+const useInput = (validateValue: { (value: string): boolean }) => {
+	const [enteredValue, setEnteredValue] = useState("");
+	const [isTouched, setIsTouched] = useState(false);
+
+	const valueIsValid = validateValue(enteredValue);
+	const hasError = !valueIsValid && isTouched;
+
+	const valueChangeHandler = (event:React.ChangeEvent<HTMLInputElement>) => {
+		setEnteredValue(event.target.value);
+	};
+
+	const inputBlurHandler = () => {
+		setIsTouched(true);
+	};
+
+	const reset = () => {
+		setEnteredValue("");
+		setIsTouched(false);
+		console.log("asd")
+	};
+
+	return {
+		value: enteredValue,
+		isValid: valueIsValid,
+		hasError,
+		valueChangeHandler,
+		inputBlurHandler,
+		reset,
+	};
+};
+
+export default useInput;

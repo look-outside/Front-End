69cf3d3d9ca63308f27f9001f340c898f51d05bf
진현주 <97022695+realzu@users.noreply.github.com>
2022-08-10 19:54:08 +0900

    STYLE : admin 중복 스타일 컴포넌트 처리
    중복된 버튼 컴포넌트) styled-components 분리 작업

---
+++ src/styles/Admin.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Del = styled.button`
+    color: white;
+    background-color: skyblue;
+    border: 2px solid skyblue;
+    border-radius: 5px;
+    margin-left: 1em;
+    &:hover {
+        cursor: pointer;
+    }    
+`;
+
+export default Del;

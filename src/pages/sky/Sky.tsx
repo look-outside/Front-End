5b7d1304c77a4d9813932cb303915591045d63d7
진현주 <97022695+realzu@users.noreply.github.com>
2022-08-10 20:07:44 +0900

    ADD : 오늘의 하늘 design + MSW api
    Today's Sky page design + MSW api link

--- src/pages/sky/Sky.tsx
+++ src/pages/sky/Sky.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { MdEdit } from 'react-icons/md';
+import * as c from '../../styles/Category';
 
-const Sky = () => {
-  return (
-    <div>Sky</div>
-  )
+interface Post {
+    artNo: number,
+    artTitle: string,
+    useNick: string,
+    useDis: string
 }
 
-export default Sky
+const Sky = () => {
+    const [data, setData] = useState<Post[]>([]);
+    
+    useEffect(() => {
+        axios.get('/articles')
+        .then(res => {
+            setData(res.data);
+        })
+    },[])
+
+    return (
+        <c.Container>
+            <c.Title>오늘의 하늘</c.Title>
+            <c.Btn><MdEdit />글쓰기</c.Btn>
+            <c.Filter>지역 선택 필터</c.Filter>{/* 추후 추가 */}
+
+            <c.Imgs>
+                {
+                    data && (
+                        data.map((art,i) => (
+                            <c.Card key={i}>
+                                <c.Img src='/test.jpg' alt='test1' />
+                                <p>{art.artTitle}</p>
+                                <span>{art.useNick}&nbsp;</span>
+                                <span id='dis'>{art.useDis}</span>
+                            </c.Card>
+                        ))
+                    )
+                }
+            </c.Imgs>
+        </c.Container>
+    );
+};
+
+export default Sky;

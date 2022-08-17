524a506bda876ae50c442af6bd8e2daad729c19c
이시형 <tlgud585@gmail.com>
2022-08-18 02:05:20 +0900

    UPDATE : DetailPost
    댓글등록, 내용 파일 추가

--- src/pages/detail_post/DetailPost.tsx
+++ src/pages/detail_post/DetailPost.tsx
@@ -2,12 +2,26 @@ import React from "react";
 import styled from "styled-components";
 import Comments from "../../components/comment/Comments";
 import UploadComment from "../../components/comment/UploadComment";
+import PostContent from "../../components/post_content/PostContent";
+
+const DETAILPOST = {
+	artNo: 1,
+	useNo: 1,
+	artSubject: "공휴일",
+	artContents: "# Hello, *world*!",
+	artCreated: "22.08.16 01:35:35",
+	artCategory: 1,
+	regNo: "0101",
+	artWSelect: 3,
+};
 
 const DetailPost = () => {
+	// 요기서 api를 호출하고 뿌려준다?
 	return (
 		<ContainerTag>
+			<PostContent content={DETAILPOST}/>
 			<Comments />
-            <UploadComment/>
+			<UploadComment />
 		</ContainerTag>
 	);
 };
@@ -17,7 +31,7 @@ export default DetailPost;
 const ContainerTag = styled.div`
 	padding: 0 2em;
 	display: grid;
-    row-gap: 1em;
+	row-gap: 1em;
 	@media screen and (min-width: 1160px) {
 		max-width: 1160px;
 		margin: 0 auto;

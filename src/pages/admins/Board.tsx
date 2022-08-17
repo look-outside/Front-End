736f1bfc1557c67336ceb5084a5995800f0d950f
진현주 <97022695+realzu@users.noreply.github.com>
2022-08-17 23:45:33 +0900

    FEAT : admin's article list category, delete work
    지역은 백엔드 api 추가되면 수정 예정

--- src/pages/admins/Board.tsx
+++ src/pages/admins/Board.tsx
@@ -1,44 +1,87 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import Swal from 'sweetalert2';
 import Del from '../../styles/Admin';
 import * as i from '../../styles/mypage/TabInner';
 
 interface Article {
-    boardNo: number,
-    title: string,
-    district: string,
-    date: string
+    artNo: number,
+    artSubject: string,
+    regNo: string,
+    artCreated: string
 }
 
 const Board = () => {
     const [article, setArticle] = useState<Article[]>([]);
+    const [selected,setSelected] = useState ('4');
+
+    const cks :number[] = [];
 
     useEffect(() => {
-        axios.get('/article')
+        if (selected === '4') {
+            axios.get('/manager/article')
+            .then(res => {
+                setArticle(res.data.data.content);
+            })
+        } else {
+            axios.get(`/manager/article/${selected}`)
+            .then(res => {
+                setArticle(res.data.data.content);
+            })
+        }
+        
+    },[selected])
+
+    const checks = (ck: boolean, no: number) => { //삭제글 선택
+        if (ck) {
+            cks.push(no);
+        }else {
+            cks.splice(cks.indexOf(no), 1);
+        }
+    }
+
+    const del = () => { //글 삭제
+        return axios.delete(`/manager/article/${cks}`)
         .then(res => {
-            setArticle(res.data);
-        })
-    },[])
+            Swal.fire({
+                icon: 'success',
+                text: '삭제 완료!',
+                showConfirmButton: false,
+                timer: 1000
+            })
+            .then(() => window.location.reload())            
+        });
+    }
+
+    const cateChange = (cate :string) => {
+        setSelected(cate);
+    }
 
     return (
         <i.Outline>
-            {/* 필터 추가 */}
             <i.TabTitle>게시글 목록</i.TabTitle>
+            <Sel onChange={(e) => cateChange(e.target.value)} value={selected}>
+                <option value='4'>전체</option>
+                <option value='0'>데일리룩</option>
+                <option value='1'>오늘 뭐 입지</option>
+                <option value='2'>오늘의 하늘</option>
+                <option value='3'>오늘의 모임</option>
+            </Sel>
             <Col>
                 <li id='head1'>글 제목</li>
                 <li>지역</li>
                 <li>날짜</li>
-                <Del>삭제</Del>
+                <Del onClick={()=> del()}>삭제</Del>
             </Col>
             {
                 article && (
                     article.map((post,i) => (
                         <Cnt key={i}>
-                            <li id='head2'>{post.title}</li>
-                            <li>{post.district}</li>
-                            <li>{post.date}</li>
-                            <input type='checkbox'/>
+                            <li id='head2'>{post.artSubject}</li>
+                            <li>{post.regNo}</li>
+                            <li>{post.artCreated.slice(0,8)}</li>
+                            <input type='checkbox'  onChange={(e)=>{checks(e.target.checked, post.artNo)}}/>
                         </Cnt>
                     ))
                 )
@@ -120,4 +163,14 @@ const Cnt = styled.ul`
     @media screen and (max-width: 401px){
         li{ width: 26%; }
     }/* 세부반응형 추후 추가 */
+`;
+
+const Sel = styled.select`
+    margin: 1em;
+    width: 15%;
+    padding: 0.5em;
+    border-radius: 5px;
+    &:focus {
+        border: 1px solid skyblue;
+    }
 `;

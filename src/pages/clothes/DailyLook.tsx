39cd78e22e0cd5146f6fb8b9f745cf05d181c234
진현주 <97022695+realzu@users.noreply.github.com>
2022-08-19 23:28:11 +0900

    FEAT : 데일리룩, 오늘의하늘 type, api, component works
    Post 타입 이동
    지역 필터, Pagination 추가
    API 데이터 작업 처리 (API 완성 대기중)

--- src/pages/clothes/DailyLook.tsx
+++ src/pages/clothes/DailyLook.tsx
@@ -1,44 +1,47 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { MdEdit } from 'react-icons/md';
+import CateHeader from '../../components/CateHeader';
+import Pagination from '../../components/Pagination';
 import * as c from '../../styles/Category';
-
-interface Post {
-    artNo: number,
-    artTitle: string,
-    useNick: string,
-    useDis: string
-}
+import { PageT, Post } from '../../types/types';
 
 const DailyLook = () => {
     const [data, setData] = useState<Post[]>([]);
-    
+
+    const [region, setRegion] = useState<string>('');
+    const getRegionHandler = (reg :string) => { setRegion(reg) }
+
+    const [page, setPage] = useState<PageT>({});
+    const [curPage, setCurPage] = useState(1);
+
     useEffect(() => {
-        axios.get('/articles')
+        // axios.get(`/article/list/1/${region}`, {params: {page: (curPage-1)}})
+        axios.get('/article/list/1/0101', {params: {page: (curPage-1)}}) //임시
         .then(res => {
-            setData(res.data);
+            setData(res.data.data.list)
+            setPage(res.data.data.pageable)
         })
-    },[])
+    },[region,curPage])
 
     return (
         <c.Container>
-            <c.Title>데일리룩</c.Title>
-            <c.Btn><MdEdit />글쓰기</c.Btn>
-            <c.Filter>지역 선택 필터</c.Filter>{/* 추후 추가 */}
+            <CateHeader category='데일리룩' onGetRegionNumber={getRegionHandler} />
             <c.Imgs>
-                {
-                    data && (
-                        data.map((art,i) => (
-                            <c.Card key={i}>
-                                <c.Img src='/test.jpg' alt='test1' />
-                                <p>{art.artTitle}</p>
-                                <span>{art.useNick}&nbsp;</span>
-                                <span id='dis'>{art.useDis}</span>
-                            </c.Card>
-                        ))
-                    )
-                }
+                {data && (
+                    data.map((art,i) => (
+                        <c.Card key={i}>
+                            <c.Img src='/test.jpg' alt='test1'/>
+                            <p>{art.artSubject}</p>
+                            <span>{art.useNick}&nbsp;</span>
+                            <span id='dis'>{art.regAddr1} {art.regAddr2}</span>
+                        </c.Card>
+                    ))
+                )}
             </c.Imgs>
+            {data && (
+                    <Pagination curPage={curPage} setCurPage={setCurPage} totalPage={page.totalPages} totalCount={page.totalElements} size={page.size} pageCount={5}/>
+            )}
         </c.Container>
     );
 };

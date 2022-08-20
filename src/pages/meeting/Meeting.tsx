35491e1c6c05bd887f7520c86266a31e356640c5
이시형 <tlgud585@gmail.com>
2022-08-21 05:46:54 +0900

    UPDATE : pagination, Freeposts 추가

--- src/pages/meeting/Meeting.tsx
+++ src/pages/meeting/Meeting.tsx
@@ -2,20 +2,42 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import CateHeader from "../../components/CateHeader";
-
-
+import Pagination from "../../components/Pagination";
+import { PageT } from "../../types/types";
+import axios from "axios";
+import { getGategoryPosts } from "../../services/post";
+import FreePosts from "../../components/free_posts/FreePosts";
 const Meeting = () => {
 	const [region, setRegion] = useState<string>("");
+	const [page, setPage] = useState<PageT>();
+	const [curPage, setCurPage] = useState(1);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [posts, setPosts] = useState<[]>([]);
 	const getRegionHandler = (reg: string) => {
 		setRegion(reg);
 	};
-	useEffect(()=>{
-		console.log(region)
-	},[region])
+	useEffect(() => {
+		setIsLoading(true);
+		const getPosts = async () => {
+			const res = await getGategoryPosts(0, region, curPage);
+			console.log(res);
+			setPosts(res.data.data.list);
+			setPage(res.data.data.pageble);
+		};
+		getPosts();
+		setIsLoading(false);
+	}, [region, curPage]);
 	return (
 		<ContainerTag>
-			<CateHeader categoryNum={0} category="오늘의 모임" onGetRegionNumber={getRegionHandler}/>
-			
+			<CateHeader
+				categoryNum={0}
+				category="오늘의 모임"
+				onGetRegionNumber={getRegionHandler}
+			/>
+			{isLoading ? (
+				<p>로딩중</p>
+			) : <FreePosts posts={posts} path="/today_meeting"/>}
+			{page && <Pagination curPage={curPage} setCurPage={setCurPage} totalPage={page.totalPages} totalCount={page.totalElements} size={page.size} pageCount={3}/>}
 		</ContainerTag>
 	);
 };
@@ -34,6 +56,6 @@ export const WrapperTag = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
-  row-gap: 2em;
+	row-gap: 2em;
 	width: 100%;
 `;

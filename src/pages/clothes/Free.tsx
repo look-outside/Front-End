d30586fdf659e60228bdbb09d5a885a47428c48f
이시형 <tlgud585@gmail.com>
2022-08-21 15:08:35 +0900

    UPDATE : 게시글 목록 추가

--- src/pages/clothes/Free.tsx
+++ src/pages/clothes/Free.tsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import CateHeader from "../../components/CateHeader";
+import FreePosts from "../../components/free_posts/FreePosts";
+import LoadingSpinner from "../../components/LoadingSpinner";
+import Pagination from "../../components/Pagination";
+import { getGategoryPosts } from "../../services/post";
+import { PageT } from "../../types/types";
+import { ContainerTag } from "../meeting/Meeting";
 
 const Free = () => {
-    return (
-        <div>
-            오늘 뭐 입지?
-        </div>
-    );
+	const [region, setRegion] = useState<string>("");
+	const [page, setPage] = useState<PageT>();
+	const [curPage, setCurPage] = useState(1);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [posts, setPosts] = useState<[]>([]);
+	const getRegionHandler = (reg: string) => {
+		setRegion(reg);
+	};
+
+	useEffect(() => {
+		const getPosts = async () => {
+			setIsLoading(true);
+			const res = await getGategoryPosts(1, region, curPage);
+			setPosts(res.data.data.list);
+			setPage(res.data.data.pageable);
+			setIsLoading(false);
+		};
+		getPosts();
+	}, [region, curPage]);
+	return (
+		<ContainerTag>
+			<CateHeader
+				categoryNum={1}
+				category="오늘 뭐 입지?"
+				onGetRegionNumber={getRegionHandler}
+			/>
+			{isLoading ? (
+				<LoadingSpinner />
+			) : (
+				<FreePosts posts={posts} path="/today_meeting" />
+			)}
+			{page && posts.length !== 0 &&(
+				<Pagination
+					curPage={curPage}
+					setCurPage={setCurPage}
+					totalPage={page.totalPages}
+					totalCount={page.totalElements}
+					size={page.size}
+					pageCount={3}
+				/>
+			)}
+		</ContainerTag>
+	);
 };
 
 export default Free;

cf3b4155c5da71de35930074ffeddd391841da73
이시형 <tlgud585@gmail.com>
2022-08-24 00:12:43 +0900

    ADD : Private , Public 라우트 추가 및 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React, { useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 
 import Clothes from "./pages/clothes/Clothes";
 import FindId from "./pages/find_id/FindId";
 import FindPassword from "./pages/find_password/FindPassword";
 import NewPassword from "./pages/find_password/NewPassword";
-
 
 import Home from "./pages/home/Home";
 import Join from "./pages/Join/Join";
@@ -29,54 +28,68 @@ import Free from "./pages/clothes/Free";
 import DetailPost from "./pages/detail_post/DetailPost";
 import SnsLogin from "./pages/SnsLogin";
 import Weather from "./components/Weather";
-import SnsInfo from './pages/myPage/SnsInfo';
+import SnsInfo from "./pages/myPage/SnsInfo";
 import { onSilentRefresh } from "./services/user";
 import authStore from "./store/authStore";
 import axios from "axios";
+import PrivateRoute from "./utils/PrivateRoutes";
+import PublicRoutes from "./utils/PublicRoutes";
 
 function App() {
-	const {token} = authStore()
-	useEffect(()=>{
-		axios.defaults.headers.common["Authorization"] = `Bearer ${token}`
-		onSilentRefresh()
-	},[token])
-	
+	const { token } = authStore();
+	useEffect(() => {
+		axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+		onSilentRefresh();
+	}, [token]);
+
 	return (
 		<BrowserRouter>
 			<GlobalStyles />
 			<Routes>
 				<Route path="/" element={<Layout />}>
+					{/* public routes */}
 					<Route index element={<Home />} />
 					<Route path="today_clothes">
 						<Route path="main" element={<Clothes />} />
 						<Route path="dailylook" element={<DailyLook />} />
+						<Route path="dailylook/:id" element={<DetailPost />} />
 						<Route path="free" element={<Free />} />
+						<Route path="free/:id" element={<DetailPost />} />
 					</Route>
 					<Route path="today_sky" element={<Sky />} />
-					<Route path="today_meeting" element={<Meeting />}/>
-					<Route path="today_meeting/:id" element={<DetailPost/>}/>
-					<Route path="login" element={<Login/>}/>
-					<Route path="join" element={<Join/>}/>
-					<Route path="upload_post" element={<UploadPost/>}/>	
+					<Route path="today_sky/:id" element={<DetailPost />} />
+
+					<Route path="today_meeting" element={<Meeting />} />
+					<Route path="today_meeting/:id" element={<DetailPost />} />
+
 					<Route path="find">
-						<Route path="id" element={<FindId/>}/>
-						<Route path="password" element={<FindPassword/>}/>
+						<Route path="id" element={<FindId />} />
+						<Route path="password" element={<FindPassword />} />
 					</Route>
-					<Route path="edit_password" element={<NewPassword/>}/>
-					<Route path="my" element={<MyPage />}>
-						<Route path='info' element={<PwCheck />} />
-						<Route path='info/update' element={<Info />} />
-						<Route path='snsInfo' element={<SnsInfo/>} />
-						<Route path='posts' element={<Posts/>} />
-						<Route path='comments' element={<Comments/>} />
-						<Route path='delete' element={<Delete/>} />
+					<Route path="edit_password" element={<NewPassword />} />
+					<Route element={<PublicRoutes />}>
+						<Route path="login" element={<Login />} />
+						<Route path="join" element={<Join />} />
+						<Route path="/oauth/redirect" element={<SnsLogin />} />
 					</Route>
-					<Route path="admin" element={<Admin/>}>
-						<Route path="users" element={<Users/>} />
-						<Route path="board" element={<Board/>} />
+					{/* protect */}
+					<Route element={<PrivateRoute />}>
+						<Route path="upload_post" element={<UploadPost />} />
+						<Route path="my" element={<MyPage />}>
+							<Route path="info" element={<PwCheck />} />
+							<Route path="info/update" element={<Info />} />
+							<Route path="snsInfo" element={<SnsInfo />} />
+							<Route path="posts" element={<Posts />} />
+							<Route path="comments" element={<Comments />} />
+							<Route path="delete" element={<Delete />} />
+						</Route>
+						<Route path="admin" element={<Admin />}>
+							<Route path="users" element={<Users />} />
+							<Route path="board" element={<Board />} />
+						</Route>
+						<Route path="Weather" element={<Weather />} />
 					</Route>
-					<Route path="/oauth/redirect" element={<SnsLogin/>}/>
-					<Route path="Weather" element={<Weather/>}/>
+					<Route path="*" element={<Navigate to="/" replace />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>

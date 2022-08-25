9e8401aab7ededb2965e6b2f9d09d8c15d665b7b
이시형 <tlgud585@gmail.com>
2022-08-25 20:24:52 +0900

    UPDATE : 게시물 등록, 상세 페이지, 게시물 개수

--- src/services/post.ts
+++ src/services/post.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+import { unescape } from "html-escaper";
 interface Props {
 	useNo: number;
 	enteredTitle: string;
@@ -18,39 +18,53 @@ export const postUpload = async ({
 	selectedWeather,
 }: Props) => {
 	const form = new FormData();
-	form.append("useNo", `${useNo}`);
-	form.append("artSubject", enteredTitle);
-	form.append("artContents", enteredWrite);
-	form.append("artCategory", `${categoryNum}`);
-	form.append("regNo", selectedRegion);
-	form.append("artWSelect", `${selectedWeather}`);
-	console.log(form);
-	const res = await axios.post("/article/post",form, {
-		headers: {
-			"Content-Type": "multipart/form-data",
-		},
-	});
+	form.append("multipartFiles", "");
+	form.append(
+		"articles",
+		`{"useNo":${useNo}, "artSubject": "${enteredTitle}","artContents": "${enteredWrite}","artCategory":  "${categoryNum}","regNo": "${selectedRegion}","artWSelect" : ${selectedWeather}}`
+	);
+	const res = await axios.post("/article/post", form);
 	return res;
 };
 
-//  메인 페이지 게시물 
-export const getMainPosts = async(categoryNum:number) => {
-	try{
-		const res = await axios.get(`/article/list/${categoryNum}`)
-		return res
-	}catch(error:any){
-		return error.response
+//  메인 페이지 게시물
+export const getMainPosts = async (categoryNum: number, size: number) => {
+	try {
+		const res = await axios.get(`/article/category/${categoryNum}`, {
+			params: { size },
+		});
+		return res;
+	} catch (error: any) {
+		return error.response;
 	}
-}
+};
 
 //  페이지별 게시물
-export const getGategoryPosts  = async(categoryNum:number,region:string,page:number) => {
-	try{
-		const res = await axios.get(`/article/list/${categoryNum}/${region}`,{
-			params: {page}
-		})
-		return res
-	}catch(error:any){
-		return error.response
+export const getGategoryPosts = async (
+	categoryNum: number,
+	region: string,
+	page: number
+) => {
+	try {
+		const res = await axios.get(`/article/list/${categoryNum}/${region}`, {
+			params: { page },
+		});
+		return res;
+	} catch (error: any) {
+		return error.response;
 	}
-}
+};
+
+// 상세 페이지
+export const getDetailPost = async (artNo: number) => {
+	try {
+		const res = await axios.get(`/article/${artNo}`);
+		return res;
+	} catch (error: any) {
+		return error.response;
+	}
+};
+
+//  게시물 삭제
+
+//  게시물 수정

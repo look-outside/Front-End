da949b1a18c289dee76302250efbac71608e2740
이시형 <tlgud585@gmail.com>
2022-08-30 18:04:02 +0900

    FIX : react version conflict

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-// if (process.env.NODE_ENV === 'development') {
-//   const { worker } = require('./mocks/browser');
-//   worker.start();
-// }
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root") as HTMLElement
 );

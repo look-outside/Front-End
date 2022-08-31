e6e4df95bffa106387ba87703381f2dfae39448f
이시형 <tlgud585@gmail.com>
2022-08-31 22:15:35 +0900

    UPDATE : baseurl

--- src/services/region.ts
+++ src/services/region.ts
@@ -1,6 +1,8 @@
 import axios from "axios"
+import { BASE_URL } from "../utils/axios"
+
 
 export const getRegion = async(rez:string)=>{
-	const res = await axios.get(`/region/${rez}`)
+	const res = await axios.get(`${BASE_URL}/region/${rez}`)
 	return res
 }

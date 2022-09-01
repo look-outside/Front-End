52944e6e048b4dab309cadd39bb8bd8028143dff
진현주 <97022695+realzu@users.noreply.github.com>
2022-09-02 01:42:11 +0900

    UPDATE : proxy api end point

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,9 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const apiKey = process.env.REACT_APP_WEATHER_API
-
 module.exports = function(app) {
     app.use(
     createProxyMiddleware('/api', {
-        target: `https://api.openweathermap.org/data/2.5/group?id=1835847,1841610,1843125,1845106,1845105,1845789,1845788,1841597,1902028,1846265&appid=${apiKey}&lang=kr&units=metric`,
+        target: 'https://api.openweathermap.org',
         pathRewrite: { '^/api': '' },
         changeOrigin: true,
         })
